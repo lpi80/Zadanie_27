eb6165a865978ea07bd0251dc9803c462431c827
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-05-22 17:21:11 +0200

    Format CommentsList

--- redux-demo/src/CommentsList.js
+++ redux-demo/src/CommentsList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Comment from './CommentContainer.js';
 
-const CommentsList = ({comments}) => 
-<ul>
-	{comments.map(comment => 
-	<Comment 
-		key={comment.id} 
-		text={comment.text} 
-		votes={comment.votes} 
-		{...comment}/>)}
-</ul>
+const CommentsList = ({ comments }) =>
+	<ul>
+		{comments.map(comment =>
+			<Comment
+				key={comment.id}
+				text={comment.text}
+				votes={comment.votes}
+				{...comment}
+			/>
+		)}
+	</ul>
 
 export default CommentsList;
